15bc080e744222ad7ab53f94fb5740dad9c13f16
Tilman <tliero@gmx.net>
2019-02-14 09:28:43 +0100

    Better output for button test

--- code/test/buttons.py
+++ code/test/buttons.py
@@ -9,17 +9,17 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
-    print('Start button test')
+    print('Start button test - End with Ctrl-C')
     
     while True:
-        if (GPIO.input(9) == False):
+        if (GPIO.input(10) == False):
             print('Button 1 (PREV)')
     
-        if (GPIO.input(10) == False):
-            print('Button 3 (NEXT)')
+        if (GPIO.input(9) == False):
+            print('Button 2 (PLAY/PAUSE)')
     
         if (GPIO.input(11) == False):
-            print('Button 2 (PLAY/PAUSE)')
+            print('Button 3 (NEXT)')
     
         time.sleep(0.25)
     
